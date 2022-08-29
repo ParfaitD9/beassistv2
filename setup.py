d94cf4ffd0f2923302c43d5c549218379a9901ae
Parfait Detchenou <pdetchenou@gmail.com>
2022-08-29 01:19:41 +0100

    Simplifies update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.cmd == 'install':
         print("Préparation de l'installation de beassist ...")    
-        print("Étape 3/ : Installation des dépendances ...")
+        print("Étape 1/ : Installation des dépendances ...")
         try:
             subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
         except (Exception, ) as e:
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         else:
             print('Dépendances installées avec succès. Étape 3/ accompli')
         
-        print("Étape 4/ : Vérification du fichier de configuration ...")
+        print("Étape 2/ : Vérification du fichier de configuration ...")
         if os.path.exists('.env') and os.path.isfile('.env'):
             print("Fichier de configuration déjà existant")
         else:
@@ -41,14 +41,14 @@ if __name__ == '__main__':
         print("Vérification de configuration effectif. Étape 4/ accompli")
 
         
-        print("Étape 5/ : Création des répertoires importants ...")
+        print("Étape 3/ : Création des répertoires importants ...")
         try:
             from ut1ls.orm import DOCS_PATH, COMPTA_PATH, FILES_PATH, BACKUP_PATH
         except (ImportError, ) as e:
             print("BéAssist n'à pas pu définir les répertoires utiles depuis le fichier orm")
             sys.exit()
         else:
-            for path in (DOCS_PATH, BACKUP_PATH, COMPTA_PATH, FILES_PATH):
+            for path in (DOCS_PATH, BACKUP_PATH, COMPTA_PATH):
                 try:
                     os.makedirs(path, exist_ok=True)   
                 except (Exception, ) as e:
@@ -61,7 +61,7 @@ if __name__ == '__main__':
             else:
                 print("Tous les répertoires necéssaires ont été créés avec succès. Étape 5/ accompli.")
         
-        print("Étape 6/ : Mise en place de la base de données ...")
+        print("Étape 4/ : Mise en place de la base de données ...")
         from ut1ls.orm import db, Admin, Organization, City, Customer,\
             Event, Facture, ListProduction, ListPack, Pack, PackSubTask, SubTask
         
@@ -78,6 +78,7 @@ if __name__ == '__main__':
         
         print("Fin de l'installation de beassist !!!")
         print("Configurez votre fichier .env")
+        print("Ajoutez votre fichier credentials.json dans le dossier files")
         print("Démarrez votre serveur avec la commande python app.py dans un terminal")
         print("Rendez vous dans votre terminal à l'addresse indiquée")
         
